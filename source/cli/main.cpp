9f5ffa612b8346417d3d93bb27f77bc32fb9a00e
Dongw <linsann@163.com>
2023-09-11 00:49:37 +0800

    chore: 再整理一下

--- source/cli/main.cpp
+++ source/cli/main.cpp
@@ -1,32 +1,4 @@
-﻿#include "MaaFramework/MaaAPI.h"
-#include "MaaToolKit/MaaToolKitAPI.h"
-#include "meojson/json.hpp"
-
-#include <filesystem>
-#include <fstream>
-#include <iostream>
-#include <string>
-
-struct Task
-{
-	std::string name;
-	std::string type;
-    bool enabled = true;
-	json::value param = json::object();
-};
-
-
-using TaskList = std::vector<Task>;
-
-void print_help();
-bool proc_argv(int argc, char** argv, bool& debug, std::string& adb, std::string& adb_address, int& client_type, TaskList& tasks,
-	MaaAdbControllerType& ctrl_type);
-bool app_package_and_activity(int client_type, std::string& package, std::string& activity);
-json::value homeland_param();
-void save_config(const std::string& adb, const std::string& adb_address, const int& client_type, const TaskList& tasks,
-	MaaAdbControllerType ctrl_type);
-std::string read_adb_config(const std::filesystem::path& cur_dir);
-void mpause();
+﻿#include "main.h"
 
 int main(int argc, char** argv)
 {
