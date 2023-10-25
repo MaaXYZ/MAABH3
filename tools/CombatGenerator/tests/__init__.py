704458c1093863dd2a1c5cbc22299ac7217a7b2b
BalconyJH <balconyjh@gmail.com>
2023-10-26 05:51:46 +0800

    :test_tube: 添加单元测试

--- tools/CombatGenerator/tests/__init__.py
+++ tools/CombatGenerator/tests/__init__.py
@@ -1,35 +1,69 @@
 import unittest
-import json
-from pathlib import Path
+
 from combatgenerator import (
-    Combat,
+    default_delay,
     generate_json_from_combat,
-    read_file,
-)  # 请替换 "your_module" 为你的实际模块名
+)
 
 
-class TestCombat(unittest.TestCase):
-    def setUp(self):
-        self.test_path = Path(__file__).parent.parent / "data" / "input.json"
-        with open(self.test_path, "r") as f:
-            self.data = json.load(f)
-        self.combat_instance = Combat(**self.data)
+class TestCombatMethods(unittest.TestCase):
+    def test_default_delay(self):
+        """测试 default_delay 函数是否返回正确的预延迟和后延迟。"""
+        test_cases = [
+            {"action": "Forward", "pre_delay": 0, "post_delay": 50},
+            {"action": "Backward", "pre_delay": 0, "post_delay": 50},
+            {"action": "Left", "pre_delay": 0, "post_delay": 50},
+            {"action": "Right", "pre_delay": 0, "post_delay": 50},
+            {"action": "Left Forward", "pre_delay": 0, "post_delay": 50},
+            {"action": "Left Backward", "pre_delay": 0, "post_delay": 50},
+            {"action": "Right Forward", "pre_delay": 0, "post_delay": 50},
+            {"action": "Right Backward", "pre_delay": 0, "post_delay": 50},
+            {"action": "BasicATK", "pre_delay": 0, "post_delay": 200},
+            {"action": "BasicATKCharge", "pre_delay": 0, "post_delay": 100},
+            {"action": "Evade", "pre_delay": 0, "post_delay": 150},
+            {"action": "Ultimate", "pre_delay": 0, "post_delay": 150},
+            {"action": "UltimateCharge", "pre_delay": 0, "post_delay": 100},
+            {"action": "WeaponSkill", "pre_delay": 0, "post_delay": 150},
+            {"action": "ELFSkill", "pre_delay": 0, "post_delay": 150},
+            {"action": "ExtraSkill", "pre_delay": 0, "post_delay": 100},
+        ]
+
+        for test_case in test_cases:
+            action = default_delay(str(test_case["action"]))
+            self.assertEqual(action.pre_delay, test_case["pre_delay"])
+            self.assertEqual(action.post_delay, test_case["post_delay"])
 
     def test_generate_json_from_combat(self):
-        generated_json, _ = generate_json_from_combat(self.combat_instance.combat)
+        """测试 generate_json_from_combat 函数是否返回正确的 JSON 结构。"""
+        combat_list = [
+            "Forward",
+            "Backward",
+            {"BasicATK": [0, 30]},
+            {"ExtraSkill": [30, 0]},
+        ]
+        generated_json = generate_json_from_combat(combat_list)
+        self.assertIsNotNone(generated_json)
 
-        # 进行你需要的断言检查
-        self.assertTrue(generated_json)  # 检查生成的 JSON 是否存在
-        self.assertIn(
-            "UniversalMirageCombatGenericPreheat", generated_json
-        )  # 检查特定 key 是否存在
-        # ... 添加更多断言
+        # 检查 "UniversalMirageCombatGenericPreheat" 步骤
+        self.assertIn("UniversalMirageCombatGenericPreheat", generated_json)
+        self.assertEqual(
+            generated_json["UniversalMirageCombatGenericPreheat"]["pre_delay"], 500
+        )
+        self.assertEqual(
+            generated_json["UniversalMirageCombatGenericPreheat"]["post_delay"], 1500
+        )
 
-    def test_read_file(self):
-        file_content = read_file(self.test_path)
+        # 检查 "next" 字段和下一个步骤的键是否匹配
+        keys = list(generated_json.keys())
+        for i in range(len(keys) - 1):  # 跳过最后一个元素
+            current_key = keys[i]
+            next_key = keys[i + 1]
+            self.assertIn(next_key, generated_json[current_key]["next"])
 
-        # 进行你需要的断言检查
-        self.assertTrue(file_content)
+        # 检查最后一个步骤的 "next" 字段应该只包含 "UniversalMirageCombatFinish"
+        self.assertEqual(
+            generated_json[keys[-1]]["next"], ["UniversalMirageCombatFinish"]
+        )
 
 
 if __name__ == "__main__":
